5c79087495ed6eb8e6c3d758bda346e0cb8ef176
vnay-dev <cleancode4dev@gmail.com>
2022-03-23 16:39:26 +0530

    added props to input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField/InputField";
 
-function App() {
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<string>("");
+  console.log(tasks)
+
   return (
     <div className="App">
       <span className="title">Checklist</span>
-      <InputField />
+      <InputField tasks={tasks} setTasks={setTasks} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,9 +1,19 @@
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  tasks: string;
+  setTasks: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ tasks, setTasks }) => {
   return (
     <form className="form">
-      <input type={"text"} placeholder="Enter task" />
+      <input
+        type={"text"}
+        placeholder="Enter task"
+        value={tasks}
+        onChange={(e) => setTasks(e.target.value)}
+      />
       <button type="submit">Add</button>
     </form>
   );
